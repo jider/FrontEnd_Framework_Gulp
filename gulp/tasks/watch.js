de5d2af57a1bfde0e5a50545c6735336078a3a00
Nacho <jdiaz@mega-media.es>
2015-08-03 14:21:41 +0200

    [Default Task] Watch framework changes

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -10,5 +10,6 @@ var config 	= require('../config');
 // Watchify monitoriza y recompila nuestro JS al vuelo, por lo que no es necesario crear un 'gulp.watch' sobre estos ficheros
 gulp.task('watch', ['watchify', 'browserSync'], function() {
 	gulp.watch(config.sass_lib.src, ['sass:lib']);
+	gulp.watch(config.sass_fw.src, ['sass:fw']);
 	gulp.watch(config.markup.src, ['markup']);
 });
